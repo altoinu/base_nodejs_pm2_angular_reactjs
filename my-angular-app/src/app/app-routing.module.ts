3133a86357b084112fc57237d4c1cc1c96417268
Kaoru Kawashima <altoinu@users.noreply.github.com>
2020-03-02 19:43:18 -0500

    initial commit angular stuff

--- my-angular-app/src/app/app-routing.module.ts
+++ my-angular-app/src/app/app-routing.module.ts
@@ -1,8 +1,30 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { RandomCoolComponent } from './components/random-cool.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'about/:superduperparam',
+    component: RandomCoolComponent,
+    data: {
+      blah: {
+        text: '(data from app-routing) really cool app, yoyo',
+        foobar: 'woot'
+      }
+    }
+  },
+  {
+    path: '',
+    component: RandomCoolComponent,
+    data: {
+      blah: {
+        text: '(data from app-routing) really cool app, yo',
+        foobar: 'woot'
+      }
+    }
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
