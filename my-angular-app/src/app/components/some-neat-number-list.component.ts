ed8e825fc583b37cd75327de95aca75301289014
Kaoru Kawashima <altoinu@users.noreply.github.com>
2020-03-05 22:26:41 -0500

    About component, route params, code organize

--- my-angular-app/src/app/components/some-neat-number-list.component.ts
+++ my-angular-app/src/app/components/some-neat-number-list.component.ts
@@ -1,30 +1,30 @@
 import {
-	Component,
-	OnInit,
-	Input,
-	AfterContentInit
+    Component,
+    OnInit,
+    Input,
+    AfterContentInit
 } from '@angular/core';
 import { NumberSymbol } from '@angular/common';
 
 @Component({
-	selector: 'some-neat-number-list',
-	templateUrl: 'some-neat-number-list.component.html'
-	//styleUrls: ''
+    selector: 'some-neat-number-list',
+    templateUrl: './some-neat-number-list.component.html'
+    //styleUrls: ''
 })
 export class SomeNeatNumberList implements OnInit, AfterContentInit {
 
-	@Input() numbers: object[];
+    @Input() numbers: object[];
 
-	constructor() {
+    constructor() {
 
-	}
+    }
 
-	ngOnInit() {
+    ngOnInit() {
 
-	}
+    }
 
-	ngAfterContentInit() {
+    ngAfterContentInit() {
 
-	}
+    }
 
 }
