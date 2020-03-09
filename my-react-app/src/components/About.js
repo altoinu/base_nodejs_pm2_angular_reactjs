68d801416abef11d675bdb42db3b05cd5bc1f23d
Kaoru Kawashima <altoinu@users.noreply.github.com>
2020-03-09 01:28:57 -0400

    code organized

--- my-react-app/src/components/About.js
+++ my-react-app/src/components/About.js
@@ -1,61 +1,73 @@
 import React from 'react';
 import {
-	useHistory,
-	useLocation,
-	useParams
+    useHistory,
+    useLocation,
+    useParams
 } from "react-router-dom";
 
 class AboutInternalComponent extends React.Component {
 
-	render() {
+    componentDidMount() {
 
-		return (
-			<div>
-				<p>This is some really nice About page.<br />
-				We got param from URL /about/:superduperparam - {this.props.superduperparam}</p>
-				<p>Buwahahaha, and another normally passed<br />this.props.someprop: {this.props.someprop}</p>
-			</div>
-		);
+        console.log('About componentDidMount');
 
-	}
+    }
+
+    componentWillUnmount() {
+
+        console.log('About componentWillUnmount');
+
+    }
+
+    render() {
+
+        return (
+            <div>
+                <p>This is some really nice About page.<br />
+                    We got param from URL /about/:superduperparam - {this.props.superduperparam}</p>
+                <p>Buwahahaha, and another normally passed<br />this.props.someprop: {this.props.someprop}</p>
+            </div>
+        );
+
+    }
 
 }
 
 function About(props) {
 
-	let history = useHistory();
-	let location = useLocation();
+    let history = useHistory();
+    let location = useLocation();
 
-	let params = useParams();
-	//let superduperparam = params.superduperparam;
-	let {superduperparam} = useParams();
+    let params = useParams();
+    //let superduperparam = params.superduperparam;
+    let {superduperparam} = useParams();
 
-	console.log('history in About', history);
-	console.log('location in About', location);
-	console.log('params in About', params);
+    console.log('history in About', history);
+    console.log('location in About', location);
+    console.log('params in About', params);
 
-	/* hmm, useParams doesn't work with this
-	return class extends React.Component {
+    /* hmm, useParams doesn't work with this
+    return class extends React.Component {
 
-		render() {
+        render() {
 
-			return (
-				<div>
-					<p>This is some really nice About page. We got superduperparam: {superduperparam}</p>
-					<p>Buwahahaha</p>
-				</div>
-			);
+            return (
+                <div>
+                    <p>This is some really nice About page. We got superduperparam: {superduperparam}</p>
+                    <p>Buwahahaha</p>
+                </div>
+            );
 
-		}
+        }
 
-	};
-	*/
+    };
+    */
 
-	return (
-		<AboutInternalComponent superduperparam={superduperparam} someprop={props.someprop}>
-			{/* Passing useParams to internally defined class */}
-		</AboutInternalComponent>
-	);
+    return (
+        <AboutInternalComponent superduperparam={superduperparam} someprop={props.someprop}>
+            {/* Passing useParams to internally defined class */}
+        </AboutInternalComponent>
+    );
 
 }
 
