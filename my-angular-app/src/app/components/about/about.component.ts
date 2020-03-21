1dc203d42cfb9d61b8f5f03bdb492052e94a1478
Kaoru Kawashima <altoinu@users.noreply.github.com>
2020-03-21 13:52:06 -0400

    Notes on route parameters

--- my-angular-app/src/app/components/about/about.component.ts
+++ my-angular-app/src/app/components/about/about.component.ts
@@ -4,8 +4,13 @@ import {
   OnInit,
   OnDestroy
 } from '@angular/core';
-import { ActivatedRoute, Router } from '@angular/router';
+import {
+  ActivatedRoute,
+  Router,
+  ParamMap
+} from '@angular/router';
 import { Subscription } from 'rxjs';
+import { switchMap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-about',
@@ -21,7 +26,10 @@ export class AboutComponent implements OnInit, OnDestroy {
   private routeParamUpdate: Subscription;
 
   // RouteParams ROUTER_PROVIDERS
-  constructor(private route: ActivatedRoute, private router: Router) {
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router
+  ) {
 
     // data passed from route
     // https://yakovfain.com/2015/11/11/angular-2-passing-data-to-routes/
@@ -38,13 +46,35 @@ export class AboutComponent implements OnInit, OnDestroy {
     this.someprop = this.route.snapshot.data.someprop;
 
     // refresh when route params change
+    // https://angular.io/start/start-routing#using-route-information
+    this.routeParamUpdate = this.route.paramMap.subscribe(params => {
+
+      this.superduperparam = params.get('superduperparam');
+      console.log(this.superduperparam);
+
+    });
+
+    /*
     // https://medium.com/@mvivek3112/reloading-components-when-change-in-route-params-angular-deed6107c6bb
     // https://stackoverflow.com/questions/53680613/reload-component-when-change-parts-routing-in-angular
     this.routeParamUpdate = this.route.params.subscribe(params => {
 
       this.superduperparam = this.route.snapshot.paramMap.get('superduperparam');
+      console.log(this.superduperparam);
 
     });
+    */
+
+    /*
+    Observable
+    // https://angular.io/guide/router#activated-route-in-action
+    this.route.paramMap.pipe(
+      switchMap((params: ParamMap) => {
+        console.log('yay');
+        this.superduperparam = params.get('superduperparam');
+      })
+    );
+    */
 
   }
 
